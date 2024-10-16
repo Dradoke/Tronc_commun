acc678153527031d54b4188cf95cd7ecb9f3555e
Dradoke <nathan.gaudouin76190@gmail.com>
2024-10-16 10:32:54 +0200

    ft_atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -1,25 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_atoi.str                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngaudoui <marvin@42.com>                   +#+  +:+       +#+        */
+/*   By: ngaudoui <marvin@42.strom>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/15 23:59:03 by ngaudoui          #+#    #+#             */
+/*   strreated: 2024/10/15 23:59:03 by ngaudoui          #+#    #+#             */
 /*   Updated: 2024/10/15 23:59:03 by ngaudoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int     ft_atoi(char *c)
+int     ft_atoi(char *str)
 {
-        int i;
+        int     i;
+        int     negatif;
+        int     result;
 
         i = 0;
-        while(c[i])
+        negatif = 1;
+        result = 0;
+        while(str[i])
         {
-                if (c[i] )
+                while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
+			|| str[i] == '\f' || str[i] == '\r')
+                {
+                        i++;
+                }
+                if (str[i] == '-' || str[i] == '+')
+                {
+                        if (str[i] == '-')
+                        {
+                                negatif = -1;
+                        }
+                        i++;
+                }
+                while (str[i] >= '0' && str[i] <= '9')
+                {
+                        result = result * 10 + (str[i] - '0');
+                        i++;
+                }
+                return (result * negatif);
         }
-        
+}
+
+
+
+int     main(void)
+{
+        printf("test pour -1254879: %d", ft_atoi("      -1254879"));
 }
