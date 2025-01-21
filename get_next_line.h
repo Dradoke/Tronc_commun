ca80d5b87163c6ae29385aa0b34e7453450ef98f
Nathan Gaudouin <ngaudoui@c1r1p1.42lehavre.fr>
2025-01-21 14:23:40 +0100

    copy

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngaudoui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 12:21:28 by ngaudoui          #+#    #+#             */
+/*   Updated: 2025/01/21 14:13:01 by ngaudoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nelem, size_t elsize);
+char	*ft_free(char *buffer, char *buf);
+char	*read_file(int fd, char *buffer);
+char	*make_line(char *buffer);
+char	*next(char *buffer);
+#endif
