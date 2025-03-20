9e9cf61e081c46fdefc891ffee68031944046e42
Nathan Gaudouin <ngaudoui@c1r2p2.42lehavre.fr>
2025-03-20 16:47:21 +0100

    save

---
+++ include/push_swap.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngaudoui <ngaudoui@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 14:03:13 by ngaudoui          #+#    #+#             */
+/*   Updated: 2025/03/20 16:36:03 by ngaudoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <limits.h>
+# include <stdlib.h>
+# include "../lib/libft/libft.h"
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}	t_stack_node;
+
+//Handle errors
+
+//Stack initiation
+
+//Nodes initiation
+
+//Commands
+
+//Algorithms
+
+#endif
