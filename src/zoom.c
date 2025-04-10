8f91e0b596207b8570d7b206fad47fe3940efb7d
Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
2025-04-10 14:35:16 +0200

    change getscale

--- src/zoom.c
+++ src/zoom.c
@@ -6,7 +6,7 @@
 /*   By: ngaudoui <ngaudoui@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 17:34:33 by ngaudoui          #+#    #+#             */
-/*   Updated: 2025/04/03 17:36:48 by ngaudoui         ###   ########.fr       */
+/*   Updated: 2025/04/10 14:33:36 by ngaudoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,9 @@ void	getscale(t_data *data)
 {
 	while (data->tab.lim.xmax * data->in.zoom < (WIN_WIDTH / 2) - 20
 		&& data->tab.lim.ymax * data->in.zoom < (WIN_HEIGHT / 2) - 20
-		&& data->tab.lim.xmin * data->in.zoom < (WIN_WIDTH / 2) - 20
-		&& data->tab.lim.ymin * data->in.zoom < (WIN_HEIGHT / 2) - 20
-		&& data->tab.lim.xmax * data->in.zoom > (-WIN_WIDTH / 2) + 20
-		&& data->tab.lim.ymax * data->in.zoom > (-WIN_HEIGHT / 2) + 20
 		&& data->tab.lim.xmin * data->in.zoom > (-WIN_WIDTH / 2) + 20
 		&& data->tab.lim.ymin * data->in.zoom > (-WIN_HEIGHT / 2) + 20)
-		data->in.zoom += 0.000001;
+		data->in.zoom += 1.0 / (data->tab.lim.xmax * data->tab.lim.ymax);
 }
 
 void	autozoom(t_data *data)
