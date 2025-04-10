3ddd00024ac45948b04177182e377388f3a44f2a
Nathan Gaudouin <ngaudoui@c1r2p4.42lehavre.fr>
2025-04-10 12:32:05 +0200

    base project

---
+++ philo/src/philo.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   project.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngaudoui <ngaudoui@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/10 12:12:43 by ngaudoui          #+#    #+#             */
+/*   Updated: 2025/04/10 12:15:47 by ngaudoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philo.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 5 || argc > 6)
+		return (printf("Error: wrong number of arguments\n"));
+	(void)argv;
+}
